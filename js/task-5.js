3208d53767f89b0b9763fc0aa8a3659ab6900b63
Axeliriya <axeliriya@mail.ru>
2020-12-19 22:51:45 +0300

    Сделала 5 задачу

--- js/task-5.js
+++ js/task-5.js
@@ -7,3 +7,12 @@
 <input type="text" placeholder="Ваше имя?" id="name-input" />
 <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
 */
+
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+console.log(inputRef); 
+console.log(outputRef);
+
+inputRef.addEventListener('input', event => {
+    outputRef.textContent = event.target.value !== '' ? event.target.value :'незнакомец';
+});
